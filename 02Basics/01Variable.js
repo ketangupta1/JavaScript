5a27cd51dfcaa6209b14f7d790356f64793135f4
Ketan Gupta <56977429+ketangupta1@users.noreply.github.com>
2022-09-12 20:48:25 +0530

    Create 01Variable.js

---
+++ 02Basics/01Variable.js
@@ -0,0 +1,11 @@
+// var, let, const
+var fullName = "Ketan Kumar";
+console.log(fullName);
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+var paymentMethod;
+paymentMethod = "Credit Card";
+console.log(paymentMethod);
