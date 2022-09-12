2deb85d62860bb3175de9a85a9a9322d742eb895
Ketan Gupta <56977429+ketangupta1@users.noreply.github.com>
2022-09-12 20:48:57 +0530

    Add files via upload

---
+++ 02Basics/02VariableTwo.js
@@ -0,0 +1,20 @@
+const uid = "abcd123";
+
+var fullName = "Ketan Gupta";
+var email = "ketankumarnna@gmail.com";
+var password = "abc@123";
+var confirmPassword = "abc@123";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+console.log("Hello "+fullName);
+console.log("Your email is "+email);
+console.log("UID: "+uid)
+
+
+console.log(`      // backtick below esc button
+    With unique Id : ${uid}
+    User is : ${fullName}
+    with email address : ${email}
+    and password is : ${password}
+`)
