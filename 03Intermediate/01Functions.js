2f5ffb7578b3b1777d757950d9fc63d928e79b11
Ketan Gupta <56977429+ketangupta1@users.noreply.github.com>
2022-09-26 19:58:59 +0530

    Create 01Functions.js

---
+++ 03Intermediate/01Functions.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log("hello ketan");
+    console.log("hey there " +name)
+}
+sayHello("Rajesh");
+
+
+
+
+function namastey(name){
+    return "Hello in India"
+}
+
+var data = namastey("Tulsi");
+console.log(data);
