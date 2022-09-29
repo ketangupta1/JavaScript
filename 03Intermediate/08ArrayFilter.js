03dad65d31f481b8cc72b22111ca1adf9a6be8ea
Ketan Gupta <56977429+ketangupta1@users.noreply.github.com>
2022-09-29 18:48:21 +0530

    Add files via upload

---
+++ 03Intermediate/08ArrayFilter.js
@@ -0,0 +1,9 @@
+var testArray = [67,89,9,54,3,89,89,4,67];
+
+// console.log(testArray.fill('K'));
+// console.log(testArray.fill('K',2,5));
+
+var myNum = [3,78,89,43,76,1,38,53,56];
+
+const result = myNum.filter((num) => num > 48); // filter() will always return an array
+console.log(result)
